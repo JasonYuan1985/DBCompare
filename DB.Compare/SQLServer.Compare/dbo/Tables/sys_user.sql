7d846d167e907e2caeeb2854934f3e46422c36ba
yuan.jy <Jason_Yuan@epam.com>
2022-01-12 16:30:45 +0800

    update for SQL Server Compare whole process

---
+++ DB.Compare/SQLServer.Compare/dbo/Tables/sys_user.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[sys_user] (
+    [id]               VARCHAR (50)   NOT NULL,
+    [user_account]     VARCHAR (50)   NULL,
+    [user_name]        NVARCHAR (50)  NULL,
+    [email]            NVARCHAR (50)  NULL,
+    [address]          NVARCHAR (200) NULL,
+    [status]           BIT            NULL,
+    [create_time]      DATETIME       NULL,
+    [create_user]      VARCHAR (50)   NULL,
+    [last_update_time] DATETIME       NULL,
+    [last_update_user] VARCHAR (50)   NULL,
+    CONSTRAINT [PK_sys_user] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
